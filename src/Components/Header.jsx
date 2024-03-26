291ebae25946bf4bd2160d32c69e14d64b0d8c8d
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2024-03-26 15:21:50 -0700

    Finsihed Header
    Finished the header and implemented an image on the page.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Link from 'next/link';
 
-const Header = () => {
+const Header = ({ listItem }) => {
   return (
     <main className='headerMain'>
         <div>RL</div>
+        <ul>
+          <li><Link  className={listItem === "aboutme" ? "aboutTitle" : null} href={'/'}>ABOUT ME</Link></li>
+          <li><Link  className={listItem === "resume" ? "resumeTitle" : null}href={'/resume'}>RESUME</Link></li>
+          <li><Link  className={listItem === "projects" ? "projectsTitle" : null} href={'/experinces'}>PROJECTS</Link></li>
+        </ul>
     </main>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
 import Header from '../Components/Header'
+import Image from 'next/image';
+
+const imageStyle = {
+  width: '60vw',
+  height: 'auto',
+  textAlign: 'center',
+  margin: 'auto'
+}
 
 export default function Home() {
   return (
     <main>
-      <Header/>
+      <Header listItem={"aboutme"}/>
+      <div className='image-container'>
+        <Image
+          src="/images/computer.png"
+          width={300}
+          height={200}
+          style={imageStyle}
+          alt="Computer displaying empty code"
+        />
+      </div>
+      
     </main>
   );
 }
