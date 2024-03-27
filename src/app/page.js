292481a3728108ee08f91439d8276c7d7cffefdf
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2024-03-27 15:01:07 -0700

    About Page
    Added a title box containing my name to the About Me page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import Header from '../Components/Header'
 import Image from 'next/image';
 
-const imageStyle = {
-  width: '60vw',
-  height: 'auto',
-  textAlign: 'center',
-  margin: 'auto'
-}
+// const imageStyle = {
+//   width: '60vw',
+//   height: 'auto',
+//   textAlign: 'center',
+//   margin: 'auto'
+// }
 
 export default function Home() {
   return (
@@ -17,11 +17,14 @@ export default function Home() {
           src="/images/computer.png"
           width={300}
           height={200}
-          style={imageStyle}
+          // style={imageStyle}
           alt="Computer displaying empty code"
         />
       </div>
-      
+      <div className='titleBox'>
+        <h1>Ren Levingar</h1>
+        <h2>Web Developer</h2>
+      </div>
     </main>
   );
 }
