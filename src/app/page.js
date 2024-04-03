79b1806ffc1f9041a1cdacdffa360359c443582e
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2024-04-03 15:05:42 -0700

    SCSS Rework
    Changed the code on the SCSS to move some of the code from the about page onto the header.

--- src/app/page.js
+++ src/app/page.js
@@ -13,19 +13,6 @@ export default function Home() {
     <main>
       {/* header import with proper prop drilling */}
       <Header listItem={"aboutme"}/>
-
-      {/* title */}
-      {/* <div className='titleBox'>
-        <h1>Ren Levingar</h1>
-        <h2>Web Developer</h2>
-      </div> */}
-
-      {/* short about me section */}
-      {/* <section className='aboutMeSection'>
-        <h3>ABOUT ME</h3>
-        <p>Over two years of experience in web development, adept in delivering high-quality solutions tailored to clients' needs.</p>
-        <Image src="/images/computer.png" width={150} height={80} alt='Computer displaying empty code'/>
-      </section> */}
     </main>
   );
 }
