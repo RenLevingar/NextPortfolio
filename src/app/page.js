541d7197e7c802a9ed5bdff081223ba5639bb572
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2024-04-11 15:16:54 -0700

    More About
    Added a lot more to the about page and finished two sections fully.

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,26 @@
 import Header from '../Components/Header'
 import Image from 'next/image';
 
-// const imageStyle = {
-//   width: '60vw',
-//   height: 'auto',
-//   textAlign: 'center',
-//   margin: 'auto'
-// }
-
 export default function Home() {
   return (
     <main>
       {/* header import with proper prop drilling */}
       <Header listItem={"aboutme"}/>
+      <section className='aboutMeSection'>
+        <h1>ABOUT ME</h1>
+        <p>Over two years of experience in web development, adept in delivering high-quality solutions tailored to clients' needs.</p>
+        <Image src="/images/computer.png" width={500} height={500} alt='Computer image'/>
+      </section>
+
+      <section className='infoSectionContainer'>
+        <section className='infoSection'>
+          <p>I'm a skilled web developer with a track record of delivering top-notch websites tailored to meet client needs, leveraging my expertise in responsive design and user-eccentric optimization.</p>
+        </section>
+      </section>
+
+      <section className='personalSection'>
+        <h1>Personal Attributes</h1>
+      </section>
     </main>
   );
 }
