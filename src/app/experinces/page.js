7875f15beb6506933c45407a9e59d2ce3258fd02
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2024-04-19 13:55:26 -0700

    Pining
    Started working on pining for the projects page.

--- src/app/experinces/page.js
+++ src/app/experinces/page.js
@@ -1,16 +1,35 @@
+'use client'
 import Header from '../../Components/Header'
 import Footer from '../../Components/Footer'
+import gsap from 'gsap';
+import {useGSAP} from '@gsap/react'
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
 
 export default function Home() {
+  gsap.registerPlugin(useGSAP)
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(() => {
+    gsap.utils.toArray(".panel").forEach((panel, i) => {
+      ScrollTrigger.create({
+          trigger: panel,
+          start: "top top",
+          pin: true,
+          pinSpacing: false
+      });
+  });
+    
+  })
+
   return (
     <main>
       <Header listItem={"projects"}/>
       <section className='projectsSection'>
-        <div className='div1'>Div 1</div>
-        <div className='div2'>Div 2</div>
-        <div className='div3'>Div 3</div>
-        <div className='div4'>Div 4</div>
-        <div className='div5'>Div 5</div>
+        <div className='div1 panel'>Div 1</div>
+        <div className='div2 panel'>Div 2</div>
+        <div className='div3 panel'>Div 3</div>
+        <div className='div4 panel'>Div 4</div>
+        <div className='div5 panel'>Div 5</div>
       </section>
       <Footer listItem={"projects"}/>
     </main>
